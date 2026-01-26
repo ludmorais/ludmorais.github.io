9e825c6914cfbd5bc153c111dc9d568ff52ac6fa
ludmorais <ludmorais10@hotmail.com>
2026-01-26 16:54:25 +0000

    26-01-2026(2)

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,39 +1,5 @@
-/* 
-  document.addEventListener('DOMContentLoaded', () => { // quando o html carregar
-    fetch('../../html/components/menu.html') // pego esse documento
-      .then(res => res.text()) // quero pega-lo para usa-lo
-      .then(html => {
-        document.querySelector('.menu-container').innerHTML = html; // no html pelo esse id
-        initMenu();
-      })
-  })
 
-  function initMenu() {
-    const menuBtn = document.querySelector('.menu-btn')
-    const menu = document.querySelector('.menu-list')
-
-    toggle.addEventListener('click', () => {
-      const isOpen = menu.classList.toggle('isOpen') // se a classe existe, remove. Se nao
-      const closeBtn = menu.classList.toggle('closeBtn') // se a classe existe, remove. Se nao
-      // o toggle é o mesmo que: 
-      /*
-      let isOpen;
-      if (menu.classList.contains('is-open')) {
-        menu.classList.remove('is-open');
-        isOpen = false;
-      } else {
-        menu.classList.add('is-open');
-        isOpen = true;
-      }
-      */
 /*
- toggle.setAttribute('aria-expanded', isOpen) // const isOpen retorna false ou true 
-})
-}
-
-*/
-
-
 document.getElementById('menu-btn').addEventListener('click', () => {
   fetch('../../html/components/menu.html') // pego esse documento
     .then(res => res.text()) // quero pega-lo para usa-lo
@@ -61,24 +27,69 @@ function initMenu() {
   })
 
 }
+*/
+const menuHTML = `
+  <nav class="side-menu">
+  <div>
+    <a class="btn-icon primary" aria-expanded="true" id="close-menu">
+        <i class="fa-solid fa-close fa-lg"></i>
+    </a>
+  </div>
+  
+  <div class="menu-list">
+    <ul>
+      <li class="container-menu-link"><a href="index.html" class="menu-link">Project</a></li>
+      <li class="container-menu-link"><a href="index.html" class="menu-link">About Me</a></li>
+      <li class="container-menu-link"><a href="index.html" class="menu-link">Contact</a></li>
+    </ul>
 
+    <div class="container-social-media menu-social-media">
+      <button class="btn-icon border light"><i class="fa-brands fa-linkedin-in fa-lg"></i></button>
+      <button class="btn-icon border light"><i class="fa-brands fa-github fa-lg"></i></button>
+      <button class="btn-icon border light"><i class="fa-brands fa-medium fa-lg"></i></button>
+    </div>
+  </div>
+</nav>
+`
 
-/*menu.classList.add('menu-close')
+const container = document.querySelector('#menu-container')
+const menuBtn = document.querySelector('#menu-btn')
+const overlay = document.querySelector('#overlay')
 
-function click() {
-  if (menuList.classList.contains('menu-close')) {
-    menu.classList.remove('menu-close')
-    menu.classList.add('menu-open')
-    window.alert('funciocou!')
-
-  } else {
-    menu.classList.add('menu-close')
-    
-  }
-
-}
-
+// injetar HTML
+/*
+fetch('../../html/components/menu.html') // pego esse documento
+  .then(res => res.text()) // quero pega-lo para usa-lo
+  .then(html => {
+    container.innerHTML = html;
+  });
 */
 
+// injetar HTML
+container.innerHTML = menuHTML
+
+const sideMenu = container.querySelector('.side-menu')
+const closeBtn = container.querySelector('#close-menu')
+
+// abrir 
+menuBtn.addEventListener('click', () => {
+  sideMenu.classList.add('active')
+  overlay.classList.add('active')
+})
+
+/*menuBtn.addEventListener('click', () => {
+  if (menuBtn.classList.contains('active')) {
+    sideMenu.classList.remove('active')
+    overlay.classList.remove('active') 
+  } 
+})*/
 
 
+
+const closeMenu = () => {
+  sideMenu.classList.remove('active')
+  overlay.classList.remove('active') 
+};
+
+closeBtn.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);

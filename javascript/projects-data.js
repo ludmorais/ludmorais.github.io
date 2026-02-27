2b657a52dc364926b6d64107b9498fb00537764f
ludmorais <ludmorais10@hotmail.com>
2026-02-27 13:01:31 +0000

    update projects details

--- javascript/projects-data.js
+++ javascript/projects-data.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     // FLUERE
     id: "flue",
-    page_in_progress: true,
+    page_in_progress: false,
     // card and basic information
     project_in_progress: false,
     title: "Fluere",
@@ -12,24 +12,24 @@ export const projects = [
     image_card: "/assets/images/images_projects/fluere-project.png",
     // project details (modal)
     image_mockup: "/assets/images/images_projects/fluere-project.png",
-    overview: "Advice Education is a brazilian company xxxxxxxxxxxxxxx. <br><br> I was a sole UX/UI designer for this project. <br><br> The goal of this project was to create xxxxxxxxxxxxxx. <br>",
+    overview: "Fluere was a project created during some Product UX/UI Design course. <br><br> This project was done in pairs, and I contributed extensively with <b>UX Research</b>, especially user Research. <br><br> The goal of this project was to create a mobile application to assist patients and psychologists with remote psychotherapy sessions, with the goal to connect them quickly and easily.<br>",
     tool_figma: true,
-    tool_adobexd: true,
-    tool_html: true,
-    tool_css: true,
-    tool_js: true,
-    tool_vscode: true,
-    tool_react: true,
-    tool_typescript: true,
-    problem: "The problem was Advice Education is a brazilian company xxxxxxxxxxxxxxx. <br><br> I was a sole UX/UI designer for this project. <br><br> The goal of this project was to create xxxxxxxxxxxxxx. <br>",
-    solution: "I created something to do somethingxxxx xxxxxxxxx.",
-    image_solution: "/assets/images/image_solution/advice-student-user-flow.png"
+    tool_adobexd: false,
+    tool_html: false,
+    tool_css: false,
+    tool_js: false,
+    tool_vscode: false,
+    tool_react: false,
+    tool_typescript: false,
+    problem: "The <strong>main challenge</strong> was the difficulty patients faced in <b>finding psychologists</b> who could meet their needs <b>quickly and affordably</b>, so they could begin remote therapy. This often caused patients to abandon their search for a professional and, consequently, worsening mental health issues. At the same time, <b>some psychologists did not feel fully adapted to digital tools</b>.",
+    solution: "The solution was based on <b>making it easier</b> for <b>patients to find and schedule</b> appointments with professionals, and <b>for professionals</b>, the solution was based on creating an <b>application that would facilitate online therapy sessions</b>.<br><br> <b>Responsibilities:</b> Project mapping, UX research, interpretation and compilation of research results, assistance in creating the experience and interface solution.",
+    image_solution: "/assets/images/image_solution/fluere-solution.png"
   }, 
 
   {
     // SELBETTI
     id: "selb",
-    page_in_progress: true,
+    page_in_progress: false,
     // card and basic information
     project_in_progress: false,
     title: "Selbetti Technology",
@@ -39,18 +39,18 @@ export const projects = [
     image_card: "/assets/images/images_projects/selbetti-project.png",
     // project details (modal)
     image_mockup: "/assets/images/images_projects/selbetti-project.png",
-    overview: "Advice Education is a brazilian company xxxxxxxxxxxxxxx. <br><br> I was a sole UX/UI designer for this project. <br><br> The goal of this project was to create xxxxxxxxxxxxxx. <br>",
-    tool_figma: true,
+    overview: "Selbetti Technology is a brazilian tech company that create information techology solutions, including software, hardware and IT services. <br><br> I <b>worked in a team</b> where each person worked on a specific product, but everyone shared experiences and lessons learned.<br><br> I contributed with two products, one for ditial and eletronic signatures <b>S-Sign</b> and another for bussiness process management <b>BMPS</b><br>.",
+    tool_figma: false,
     tool_adobexd: true,
-    tool_html: true,
-    tool_css: true,
-    tool_js: true,
-    tool_vscode: true,
-    tool_react: true,
-    tool_typescript: true,
-    problem: "The problem was Advice Education is a brazilian company xxxxxxxxxxxxxxx. <br><br> I was a sole UX/UI designer for this project. <br><br> The goal of this project was to create xxxxxxxxxxxxxx. <br>",
-    solution: "I created something to do somethingxxxx xxxxxxxxx.",
-    image_solution: "/assets/images/image_solution/advice-student-user-flow.png"
+    tool_html: false,
+    tool_css: false,
+    tool_js: false,
+    tool_vscode: false,
+    tool_react: false,
+    tool_typescript: false,
+    problem: "The main challenge was to <b>implement new feautures and improve existing ones</b>, seeking the best <b>usability</b> decisions and ensuring that <b>user needs</b> are met.",
+    solution: "I followed UX and UI Design processess <b>end to end</b>. I conducted extensive <b>user reasearchm</b>, including numerous <b>usability tests</b>. I created <b>application interfacesm</b> and contributed to the development of the company's <b>Design System</b>.",
+    image_solution: "/assets/images/image_solution/selbetti-solution.png"
   },
 
   {
